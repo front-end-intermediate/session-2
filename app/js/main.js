3dfca31af55ce40533f1d949872a8ef48e45b436
DannyBoyNYC <daniel.deverell@gmail.com>
2018-06-05 19:57:37 -0400

    done

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,30 @@
 const nav = document.getElementById('main');
 const navbar = nav.querySelector('.navitems');
+const siteWrap = document.querySelector('.site-wrap');
 
-const markup = `
-    <ul>
-      ${navItems.map(
-        navItem => `<li><a href="${navItem.link}">${navItem.label}</a></li>` 
-        ).join('')}
-    </ul>
-    `;
+// const markup = `
+//     <ul>
+//       ${navItems.map(
+//         navItem => `<li><a href="${navItem.link}">${navItem.label}</a></li>` 
+//         ).join('')}
+//     </ul>
+//     `;
 
-navbar.innerHTML = markup;
+// navbar.innerHTML = markup;
+
+fetchData(null, function (content) {
+  const markup =
+    `<ul>
+    ${content.map(
+      listItem => `<li><a href="${listItem.link}">${listItem.label}</a></li>`
+    ).join('')}
+    </ul>`;
+  nav.innerHTML = markup;
+
+  // const logo = document.querySelector('#main ul li');
+  // logo.classList.add('logo');
+  // logo.firstChild.innerHTML = '<img src="img/logo.svg" />';
+})
 
 let topOfNav = nav.offsetTop;
 
@@ -23,8 +38,37 @@ function fixNav() {
   }
 }
 
-const logo = document.querySelector('#main ul li');
-logo.classList.add('logo');
-logo.firstChild.innerHTML = '<img src="img/logo.svg" />';
+
+function navigate() {
+  let newloc = window.location.hash;
+  fetchData(newloc, function (content) {
+    let newContent = content.filter( contentItem => contentItem.link == newloc );
+    siteWrap.innerHTML = `
+    <h2>${newContent[0].header}</h2>
+    ${newContent[0].image}
+    ${newContent[0].content}
+    `;
+  })
+}
+
+
+function fetchData(hash, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function () {
+    callback(JSON.parse(xhr.response));
+  };
+  
+  xhr.open('GET', 'http://localhost:3004/content', true);
+  xhr.send();
+}
+
+
+if (!location.hash) {
+  location.hash = '#watchlist';
+}
+
+navigate();
 
 window.addEventListener('scroll', fixNav);
+window.addEventListener('hashchange', navigate);
